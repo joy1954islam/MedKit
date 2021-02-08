8d334fcc323f3cee84ccaabba54ffce30416fe47
joyislam1954 <joyislam1954@gmail.com>
2021-02-08 19:44:46 +0600

    MedKit

---
+++ MedKit/Patient/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+from .models import Appointment
+
+
+class TakeAppointmentForm(forms.ModelForm):
+    class Meta:
+        model = Appointment
+        fields = ['Number','TrxID','amount']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.is_apply = True
+        user.save()
+        return user
